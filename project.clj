ece9a9f216c6bd6ee036a65f1ca7175ec4241044
graywolf <graywolfs@gmail.com>
2013-03-28 18:21:01 +0200

    changed project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject testlink2tfs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tool to migrate test cases from Testlink to TFS"
+  :url "https://github.com/ashenwolf/testlink2tfs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
